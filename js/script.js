3f598453ecbdb110589552b15b3ea60a09f13bac
unkbin <unk.bin@gmail.com>
2023-07-05 21:53:51 +0545

    some js and footer added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+var aboutUs = document.getElementById('about-us');
+aboutUs.addEventListener('click',()=>{
+    window.scrollTo(0,660)
+})
+
+var pricing = document.getElementById('pricing');
+pricing.addEventListener('click',()=>{
+    window.scrollTo(0,1122)
+})
+
+var contact = document.getElementById('contact');
+contact.addEventListener('click',()=>{
+    window.scrollTo(0,2250)
+})
+
